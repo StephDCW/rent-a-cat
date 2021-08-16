ff0be9bc1aae0df7c69feaabb3cdfcd2707d34a6
StephDCW <82096997+StephDCW@users.noreply.github.com>
2021-08-16 16:44:06 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :reservation, only: [:new, :create, :update, :destroy, :show]
+  resources :reservations, only: [:new, :create, :update, :destroy, :show]
 end
