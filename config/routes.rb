422e0c2058d06d153da8ff77c7bf8afd9981b498
O neal Maoukola <maoukola.oneal@gmail.com>
2021-08-16 17:57:32 +0200

    solve conflict again

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-<<<<<<< HEAD
-
-=======
->>>>>>> 918dbef2eeda34bdc1b8b8e5bee973b645481c1a
   resources :offers
   resources :reservations, only: [:new, :create, :update, :destroy, :show]
 end
