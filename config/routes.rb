07431063ab23386e1bbb0d6f24e472ddf5144359
Thibault Lenclos <thiblenclos@gmail.com>
2021-08-16 17:39:36 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
-  get 'reservations/new'
-  get 'reservations/create'
-  get 'reservations/destroy'
-  get 'reservations/edit'
-  get 'reservations/update'
-  get 'reservations/show'
   devise_for :users
   root to: 'pages#home'
-
   resources :reservations, only: [:new, :create, :update, :destroy, :show]
 end
