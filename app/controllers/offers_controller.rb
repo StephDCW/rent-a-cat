20ac6b64149d7f76e651182b1dffeda51ced0a83
Stephane Mouillat <stephane.mouillat@live.fr>
2021-08-17 10:24:09 +0200

    Offers controller mis en place

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,47 +1,44 @@
 class OffersController < ApplicationController
-  # def index
-  #   @offers = Offer.all
-  # end
+  def index
+    @offers = Offer.all
+  end
 
-  # def show
-  #   @offer = Offer.find(params[:id])
+  def show
+    @offer = Offer.find(params[:id])
+  end
 
-  # end
+  def new
+    @offer = Offer.new
+  end
 
-  # def new
-  #   @offer = Offer.new
-  # end
+  def create
+    @offer = Offer.new(offer_params)
+    @offer.user_id = @offer
+    if @offer.save
+      redirect_to offer_path(@offer)
+    else
+      render :new
+    end
+  end
 
-  # def create
-  #   @offer = Offer.new(offer_params)
-  #   @offer.user_id = @offer
-  #   if @offer.save
-  #     redirect_to offer_path(@offer)
-  #   else
-  #     render :new
-  #   end
-  # end
+  def update
+    @offer = Offer.find(params[:id])
+    if @offer.update_attributes(offer_params)
+      redirect_to offer_path(@offer)
+    else
+      render 'show'
+    end
+  end
 
+  def destroy
+    @offer = Offer.find(params[:id])
+    @offer.destroy
+    redirect_to root_path
+  end
 
-  # def update
-  #   @offer = Offer.find(params[:id])
-  #   if @offer.update_attributes(offer_params)
-  #     redirect_to offer_path(@offer)
-  #   else
-  #     render 'show'
-  #   end
+  private
 
-  # end
-
-  # def destroy
-  #   @offer = Offer.find(params[:id])
-  #   @offer.destroy
-  #   redirect_to root_path
-  # end
-
-  # private
-
-  # def offer_params
-  #   params.require(:offers).permit(:name, :photo, :price, :age, :description, :species )
-  # end
+  def offer_params
+    params.require(:offer).permit(:name, :price, :age, :description, :species )
+  end
 end
