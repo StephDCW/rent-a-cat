b1e6142ff9e8dea6dc3f69612ea14e5f8b6b0371
Thibault Lenclos <thiblenclos@gmail.com>
2021-08-17 11:03:38 +0200

    Update offer.rb
    changed dependent destroy

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,5 +1,5 @@
 class Offer < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, dependent: :destroy
   validates :name, :age, :race, :description, :location, presence: true
   validates :price, numericality: true, presence: true
 end
