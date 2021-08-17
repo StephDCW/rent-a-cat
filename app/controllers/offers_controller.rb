e037d4210c0fdfa276155f2af0d46970eb0fb21d
Stephane Mouillat <stephane.mouillat@live.fr>
2021-08-17 12:46:53 +0200

    Pundit set up done normalement

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,19 +1,23 @@
 class OffersController < ApplicationController
   def index
+    @offers = policy_scope(Offer).order(created_at: :desc)
     @offers = Offer.all
   end
 
   def show
     @offer = Offer.find(params[:id])
+    authorize @offer
   end
 
   def new
     @offer = Offer.new
+    authorize @offer
   end
 
   def create
     @offer = Offer.new(offer_params)
-    @offer.user_id = @offer
+    @offer.user = current_user
+    authorize @offer
     if @offer.save
       redirect_to offer_path(@offer)
     else
@@ -21,8 +25,14 @@ class OffersController < ApplicationController
     end
   end
 
+  def edit
+    @offer = Offer.find(params[:id])
+    authorize @offer
+  end
+
   def update
     @offer = Offer.find(params[:id])
+    authorize @offer
     if @offer.update(offer_params)
       redirect_to offer_path(@offer)
     else
@@ -32,6 +42,7 @@ class OffersController < ApplicationController
 
   def destroy
     @offer = Offer.find(params[:id])
+    authorize @offer
     @offer.destroy
     redirect_to root_path
   end
--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,29 +1,37 @@
 class ReservationsController < ApplicationController
   def show
     @reservation = Reservation.find(params[:id])
+    authorize @reservation
   end
 
   def new
     @reservation = Reservation.new
+    authorize @reservation
   end
 
   def create
     @reservation = Reservation.new(reservation_params)
+    @reservation.user = current_user
+    authorize @reservation
     flash[:notice] = @reservation.errors.full_messages.to_sentence unless @reservation.save
     redirect_to offers_path
   end
 
   def destroy
+    @reservation = Reservation.find(params[:id])
+    authorize @reservation
     @reservation.destroy
     redirect_to offers_path
   end
 
   def edit
     @reservation = Reservation.find(params[:id])
+    authorize @reservation
   end
 
   def update
     @reservation = Reservation.find(params[:id])
+    authorize @reservation
     @reservation.update(reservation_params)
     redirect_to offers_path
   end
